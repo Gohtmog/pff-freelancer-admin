6949a54c68239095f17a4e594883485956fc3554
jonathan <jonathan@BKE592H>
2022-12-28 12:51:07 +0100

    maj processor test

--- src/main/java/org/mycompany/processor/ProcessorSaveTest.java
+++ src/main/java/org/mycompany/processor/ProcessorSaveTest.java
@@ -4,9 +4,7 @@ package org.mycompany.processor;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.mycompany.model.CV;
 import org.mycompany.model.Test;
-import org.mycompany.repo.ICVRepository;
 import org.mycompany.repo.ITestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -33,8 +31,8 @@ public class ProcessorSaveTest implements Processor {
 		System.out.println("id : " + te.getId());
 		System.out.println("sujet : " + te.getSujet());
 		System.out.println("valide: " + te.getValide());
-		System.out.println("list candidat: " + te.getListeCandidats());
-		System.out.println("liste entreprise: " + te.getListeEntreprises());
+		System.out.println("candidat: " + te.getCandidat());
+		System.out.println("liste entreprise: " + te.getEntreprise());
 		System.out.println("class: " + te.getClass());
 
 		iTestRepository.save(te);

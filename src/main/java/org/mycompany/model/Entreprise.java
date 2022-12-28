a5408e94bda377c64b7f35c238f95d342c8eb27b
jonathan <jonathan@BKE592H>
2022-12-28 09:48:04 +0100

    rajout des @JsonIdentityInfo(property = "id", generator = PropertyGenerator.class)

--- src/main/java/org/mycompany/model/Entreprise.java
+++ src/main/java/org/mycompany/model/Entreprise.java
@@ -14,9 +14,13 @@ import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators.PropertyGenerator;
+
 @Entity
 @Table
 @Component
+@JsonIdentityInfo(property = "id", generator = PropertyGenerator.class)
 public class Entreprise {
 
 	@Id
